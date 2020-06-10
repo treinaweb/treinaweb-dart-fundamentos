e2e2ab2bccc0a101dad016917d0ebc0be15b06c1
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2020-06-10 10:08:03 -0300

    02.02_Trabalhando_Variaveis

--- main.dart
+++ main.dart
@@ -1,3 +1,13 @@
 main() {
-  print("Olá mundo!");
+  var mensagem = "Meu primeiro script com Dart";
+  int numero = 10;
+  double peso = 60.3;
+  bool vivo = true;
+  String mensagem2 = "Olá, mundo";
+  dynamic variavelDinamica = 15;
+  print(variavelDinamica);
+  variavelDinamica = "Mensagem";
+  print(variavelDinamica);
+  print(mensagem);
+  print(numero);
 }
