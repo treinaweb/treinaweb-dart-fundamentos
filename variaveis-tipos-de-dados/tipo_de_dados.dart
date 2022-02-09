bf7f98b446f6056af5e377e0c3b2b99e0ded5d3b
arielsardinha <odararepresentacoes@outlook.com.br>
2022-02-09 14:06:38 -0200

    tipo de dados

---
+++ variaveis-tipos-de-dados/tipo_de_dados.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  String nome = "Ariel";
+  int idade = 28;
+  double salario = 1.500;
+  bool casado = true;
+  casado = false;
+
+  dynamic caringa;
+  caringa = "novo valor";
+  caringa = 28;
+  caringa = false;
+}
