3a5d9160c904861ab78b4137bf71e38c25cbbcb3
arielsardinha <odararepresentacoes@outlook.com.br>
2022-02-09 15:36:18 -0200

    trabalhando com strings

---
+++ variaveis-tipos-de-dados/trabalhando_com_string.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  String nomeCompleto = 'Ariel Sardinha';
+
+  print(nomeCompleto.length);
+  print(nomeCompleto.isEmpty);
+  print(nomeCompleto.isNotEmpty);
+  print(nomeCompleto);
+  print(nomeCompleto.toUpperCase());
+  print(nomeCompleto.toLowerCase());
+  print(nomeCompleto.replaceAll('a', 'p'));
+}
