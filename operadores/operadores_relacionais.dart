80652b706f450e6d862043025e613defec3474a4
arielsardinha <odararepresentacoes@outlook.com.br>
2022-02-09 16:05:33 -0200

    operadores relacionais

---
+++ operadores/operadores_relacionais.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  int n1 = 10;
+  int n2 = 10;
+
+  // == , != , >, <, >=, <=
+
+  print(n1 == n2);
+  print(n1 != n2);
+  print(n1 > n2);
+  print(n1 < n2);
+  print(n1 >= n2);
+  print(n1 <= n2);
+}
