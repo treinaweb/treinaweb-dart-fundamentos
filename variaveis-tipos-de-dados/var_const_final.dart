995262ddfb2bf0be78b9f750f3331485451a927d
arielsardinha <odararepresentacoes@outlook.com.br>
2022-02-09 14:44:12 -0200

    const, final. var

---
+++ variaveis-tipos-de-dados/var_const_final.dart
@@ -0,0 +1,13 @@
+import "dart:io";
+
+void main(List<String> args) {
+  var curso = "Dart";
+  curso = 'JavaScript';
+  final escola = "Treinaweb";
+  // escola = "outra";
+  const pi = 3.14;
+  // pi = 25.0;
+
+  final cursoDois = stdin.readLineSync();
+  const cursoTres = 'Treinaweb 2';
+}
