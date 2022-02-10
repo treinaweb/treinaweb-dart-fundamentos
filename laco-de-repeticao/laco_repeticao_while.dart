33cf4d875bd477e28efae85041635d60fc585845
arielsardinha <odararepresentacoes@outlook.com.br>
2022-02-10 14:10:39 -0200

    do while

--- laco-de-repeticao/laco_repeticao_while.dart
+++ laco-de-repeticao/laco_repeticao_while.dart
@@ -2,9 +2,15 @@ import 'dart:io';
 
 void main(List<String> args) {
   var resposta = '';
-  while (resposta != 'sair') {
+  // while (resposta != 'sair') {
+  //   print('você digitou $resposta');
+  //   print("dite um novo valor $resposta");
+  //   resposta = stdin.readLineSync()!;
+  // }
+
+  do {
     print('você digitou $resposta');
-    print("dite um novo valor $resposta");
+    print("dite um novo valor ");
     resposta = stdin.readLineSync()!;
-  }
+  } while (resposta != "sair");
 }
