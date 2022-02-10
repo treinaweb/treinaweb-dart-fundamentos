9fcb3f4528a367fa934a6222bc187e726601c2a6
arielsardinha <odararepresentacoes@outlook.com.br>
2022-02-10 17:41:21 -0200

    listas

---
+++ tipo-de-dados-Lis-Map/tipo-List.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  const aprovados = ['Ariel', 'Daniel', 'Carlos'];
+  aprovados[1] = 'Novo valor';
+  print(aprovados);
+
+  // print(aprovados);
+  // print(aprovados.elementAt(0));
+  // print(aprovados[2]);
+  // print(aprovados.length);
+  // aprovados[0] = 'Pedro';
+  // print(aprovados);
+
+  // aprovados = ['Diego', 'Sabrina'];
+  // print(aprovados);
+}
