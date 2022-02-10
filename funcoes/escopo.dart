bc614a9e51491f23d07a2c14226fd55e0d2424ff
arielsardinha <odararepresentacoes@outlook.com.br>
2022-02-10 16:27:19 -0200

    escopo

---
+++ funcoes/escopo.dart
@@ -0,0 +1,25 @@
+const n1 = 10;
+
+void main(List<String> args) {
+  var n2 = 30;
+  // print(n1);
+
+  // secundario() {
+  //   print(n2);
+  // }
+
+  // secundario();
+
+  inicial();
+}
+
+inicial() {
+  var n3 = 60;
+  print(n1);
+
+  terceiraFuncao() {
+    print(n3);
+  }
+
+  terceiraFuncao();
+}
