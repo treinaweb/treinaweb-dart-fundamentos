12f9eb393116d56c9ef1705b0a7eddeb41c3cb2b
arielsardinha <odararepresentacoes@outlook.com.br>
2022-02-11 12:13:10 -0200

    map

---
+++ tipo-de-dados-Lis-Map/tipo-Map.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  Map contato = const {'nome': 'Ariel', 'numero': '9999-9999'};
+  contato = {'tipo': 1};
+  print(contato);
+  // contato['nome'] = 'Daniel';
+  // print(contato);
+  // contato.addAll({'cargo': 'desenvolvedo'});
+  // print(contato);
+}
