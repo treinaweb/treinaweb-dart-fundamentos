d83de60aecf2fcbee609e985248d012834c87b2c
Ariel Sardinha <59017640+arielsardinha@users.noreply.github.com>
2022-02-14 16:32:44 -0300

    fix/tipo de dados

--- variaveis-tipos-de-dados/tipo_de_dados.dart
+++ variaveis-tipos-de-dados/tipo_de_dados.dart
@@ -1,12 +1,11 @@
 void main(List<String> args) {
   String nome = "Ariel";
   int idade = 28;
-  double salario = 1.500;
+  double salario = 1500.00;
   bool casado = true;
   casado = false;
 
   dynamic caringa;
   caringa = "novo valor";
   caringa = 28;
-  caringa = false;
 }
